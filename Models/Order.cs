f35e7f4882f5ab3733755d271620601a8a19cca7
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-06 20:48:41 +0300

    Добавил атрибут [Key] для поля ID заявки, т.к. оно используется как уникальный идентификатор (первичный ключ).

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,11 +1,14 @@
-﻿namespace independent_work_AUG.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace independent_work_AUG.Models
 {
     public class Order
-    {        
+    {
+        [Key]
         public int OrderId { get; set; } // Ключевое поле заявки
         public int Number { get; set; } // Номер заявки
         public DateTime CreateDate { get; set; } // Дата создания заявки
-        public DateTime CLoseDate { get; set; } // Дата закрытия заявки
+        public DateTime CloseDate { get; set; } // Дата закрытия заявки
         public string EquipmentName { get; set; } // Наименование оборудования
         public string FaultType { get; set; } // Тип неисправности
         public string ProblemDescription { get; set; } // Описание проблемы        
