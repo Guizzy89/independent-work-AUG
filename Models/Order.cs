1af9095243b868adb9ff16e36ab9adc68ac785ee
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-06 20:44:49 +0300

    Создал сущность заявки со всеми необходимимыми полями и описанием к ним

---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace independent_work_AUG.Models
+{
+    public class Order
+    {        
+        public int OrderId { get; set; } // Ключевое поле заявки
+        public int Number { get; set; } // Номер заявки
+        public DateTime CreateDate { get; set; } // Дата создания заявки
+        public DateTime CLoseDate { get; set; } // Дата закрытия заявки
+        public string EquipmentName { get; set; } // Наименование оборудования
+        public string FaultType { get; set; } // Тип неисправности
+        public string ProblemDescription { get; set; } // Описание проблемы        
+        public DateTime UpdatedAt { get; set; } // Дата последнего обновления заявки
+        public StatusEnum Status { get; set; } // Статус заявки
+        public virtual Client Client { get; set; } // Клиент, создавший заявку
+        public virtual Employee ResponsibleEmployee { get; set; } // Ответственный сотрудник
+        public virtual ICollection<Comment> Comments { get; set; } // Комментарии к заявке
+    }
+
+    public enum StatusEnum
+    {
+        Awaiting,
+        InProgress,
+        Completed
+    }
+}
